eab5fd7702c6eb6dfeb457f063d11f776af4a618
Nichapat <6310301034@cdti.ac.th>
2023-08-09 15:13:25 +0700

    commit

--- machine/1-3-1-microwave-async.py
+++ machine/1-3-1-microwave-async.py
@@ -21,11 +21,16 @@ import asyncio
 import time
 
 async def cook(food, t):
-    
+    print(f'{time.ctime()} - Microwave ({food}): Cooking {t} seconds...')
+    await asyncio.sleep(t)
+    print(f'{time.ctime()} - Microwave ({food}): Cooking {t} Finished cooking')
+    return f'{food} is completed'    
 
 async def main():
+    coros = [cook('Rice', 5), cook('Noodle', 3), cook('Curry' , 1)]
+    result = await asyncio.gather(*coros)
+    print(f"{time.ctime()} - {result}")
     
-
 if __name__ == '__main__':
     t1 = time.time()
     asyncio.run(main())
--- machine/1-3-3-microwave-async.py
+++ machine/1-3-3-microwave-async.py
@@ -20,11 +20,19 @@
 import asyncio
 import time
 
+
 async def cook(food, t):
-    
+    print(f'{time.ctime()} - Microwave ({food}): Cooking {t} seconds...')
+    await asyncio.sleep(t)
+    print(f'{time.ctime()} - Microwave ({food}): Cooking {t} Finished cooking')
+    return f'{food} is completed'
+
 
 async def main():
-    
+    coros = [cook('Rice', 3), cook('Noodle', 3), cook('Curry', 3)]
+    for coro in asyncio.as_completed(coros):
+        result = await coro
+        print(result)
 
 if __name__ == '__main__':
     t1 = time.time()
